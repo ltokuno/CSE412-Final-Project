6bc3bf171b94d12d7b278390f95896a748476c62
DESKTOP-5T9MU5L\Scott <svandiep@asu.edu>
2020-11-11 21:56:05 -0700

    initial model commit

---
+++ models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSE412_Group17
+{
+    class Order
+    {
+        public int UserID { get; set; }
+        public int ConfirmationNumber { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int OrderID { get; set; }
+    }
+}
---
+++ models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSE412_Group17
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
